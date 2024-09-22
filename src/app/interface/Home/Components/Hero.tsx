99f75e1abf824a6cc629a11140d6d99666549514
elsayed ghibaish <elsayedghibaish@gmail.com>
2024-09-22 17:25:03 +0300

    zain-bus

--- src/app/interface/Home/Components/Hero.tsx
+++ src/app/interface/Home/Components/Hero.tsx
@@ -17,7 +17,7 @@ export default function HeroHomePage() {
         </p>
         <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
           <a
-            href="/profile"
+            href="/profile/booking"
             className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-red-600/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
           >
             <span className="relative text-base font-semibold text-red-600">
